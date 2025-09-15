8294b839181f6c1194729fce89ccde78cf821a6d
Nikhil10062006 <nikhilagarwal10june@gmail.com>
2025-09-15 23:46:03 +0530

    Added Routing to Counsellors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Components/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home";
+import CounsellorList from "./Components/CounsellorList";
+import CounsellorDetail from "./Components/CounsellorDetail";
+import ReviewForm from "./Components/ReviewForm";
+
 export default function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/counsellorList" element={<CounsellorList />} />
+        <Route path="/counsellorDetail/:id" element={<CounsellorDetail />} />
+        <Route path="/counsellorDetail/:id/review" element={<ReviewForm />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import Button from './Button.js'
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "./Button.js";
 export default function Navbar() {
   return (
     <div className="Navbar">
-       <Button>Resouces</Button>
-       <Button>About Us</Button>
-       <Button>Login/SignUp</Button>
-       <Button>Chat</Button>
-       <Button>Profile</Button>
-       <Button>Counsellors</Button>
-       <Button><i className="fa-solid fa-moon"></i>Dark Mode</Button>
+      <Link to="/counsellorList">
+        <Button>Counsellors</Button>
+      </Link>
+      <Button>Resouces</Button>
+      <Button>About Us</Button>
+      <Button>Login/SignUp</Button>
+      <Button>Chat</Button>
+      <Button>Profile</Button>
+      <Button>
+        <i className="fa-solid fa-moon"></i>Dark Mode
+      </Button>
     </div>
-  )
+  );
 }

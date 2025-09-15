5e70fd4a6413ad5e9caf9915a4c991d77c56430a
Nikhil10062006 <nikhilagarwal10june@gmail.com>
2025-09-15 18:27:00 +0530

    Added Buttons

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import Button from './Button.js'
 export default function Navbar() {
   return (
-    <div>
-      
+    <div className="Navbar">
+       <Button>Resouces</Button>
+       <Button>About Us</Button>
+       <Button>Login/SignUp</Button>
+       <Button>Chat</Button>
+       <Button>Profile</Button>
+       <Button>Counsellors</Button>
+       <Button><i className="fa-solid fa-moon"></i>Dark Mode</Button>
     </div>
   )
 }

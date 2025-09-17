d3521869dff8b9c30be2d5a17f6a9ed1e22f0abd
Swarit1212 <swaritagrawalrrkl@gmail.com>
2025-09-17 23:03:57 +0530

    added functionality to tools.jsx to open modals for each tool when clicked.

---
+++ src/Components/worksheets.jsx
@@ -0,0 +1,26 @@
+function Worksheets() {
+  return (
+    <div>
+      <h2>Guided Worksheets</h2>
+      <ul>
+        <li>
+          <a href="https://www.therapistaid.com/worksheets/stress" target="_blank" rel="noopener noreferrer">
+            Stress Management Worksheet
+          </a>
+        </li>
+        <li>
+          <a href="https://positivepsychology.com/wp-content/uploads/2020/06/Stress-Management-Worksheet.pdf" target="_blank" rel="noopener noreferrer">
+            Stress Coping Worksheet (PDF)
+          </a>
+        </li>
+        <li>
+          <a href="https://www.therapistaid.com/worksheets/gratitude-journal" target="_blank" rel="noopener noreferrer">
+            Gratitude Journal Worksheet
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Worksheets;
